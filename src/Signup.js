e44fb41de03979241e1ba747f19122f50172ae5d
Marta Jankowska <martajan218@gmail.com>
2020-07-13 13:00:29 +0200

    sign up page finished

--- src/Signup.js
+++ src/Signup.js
@@ -9,22 +9,37 @@ class Signup {
 
         this.buttonInput = document.querySelector("#signup-button");
         this.errorWrapper = document.querySelector(".message-container")
+        this.successWrapper = document.querySelector(".success-container")
     }
 
     handleEmail = (event) => {
         const email = event.target.value;
 
-        console.log('email', email);
+        validator.validateEmail(email);
+
+        const errors = validator.getErrors();
+        if (!errors.invalidEmail) {
+            validator.validateUniqueEmail(email);
+        } 
+        this.setErrorMessages();
     }
     handlePassword = (event) => {
         const password = event.target.value;
+        const passwordRepeat = this.repeatPasswordInput.value;
 
-        console.log('password', password);
+        validator.validatePassword(password);
+        validator.validateRepeatPassword(password, passwordRepeat);
+
+        this.setErrorMessages();
     }
     handleRepeatPassword = (event) => {
-        const repeatPassword = event.target.value;
+        const passwordRepeat = event.target.value;
+        const password = this.passwordInput.value;
 
-        console.log('repeatPassword', repeatPassword);
+        validator.validatePassword(password);
+        validator.validateRepeatPassword(password, passwordRepeat);
+
+        this.setErrorMessages();
     }
     
     saveData = (event) => {
@@ -41,6 +56,10 @@ class Signup {
         this.nameInput.value = "";
         this.emailInput.value = "";
         this.passwordInput.value = "";
+        this.repeatPasswordInput.value = "";
+
+        this.showSuccessMessage();
+        this.removeMessages();
     }
 
     addListeners = () => {
@@ -48,6 +67,41 @@ class Signup {
         this.passwordInput.addEventListener("input", this.handlePassword);
         this.repeatPasswordInput.addEventListener("input", this.handleRepeatPassword);
         this.buttonInput.addEventListener("click", this.saveData);
+    }
+
+    showSuccessMessage = () => {
+        this.errorWrapper.innerHTML = "";
+
+        const errorsObj = validator.getErrors();
+        const errorsStringsArr = Object.values(errorsObj);
+
+        if (errorsStringsArr.length > 0) {
+            return;
+        } 
+
+        const successMsgP = document.createElement('p');
+        successMsgP.innerHTML = 'You have successfully signed up!';
+
+        this.successWrapper.appendChild(successMsgP);
+    }
+
+    removeMessages = () => {
+        setTimeout( () => {
+            this.successWrapper.innerHTML = "";
+        }, 2000)
+    };
+
+    setErrorMessages = () => {
+        this.errorWrapper.innerHTML = "";
+        const errorsObj = validator.getErrors();
+        const errorsStringsArr = Object.values(errorsObj);
+
+        errorsStringsArr.forEach((errorStr) => {
+            const errorMsgP = document.createElement('p');
+            errorMsgP.innerHTML = errorStr;
+
+            this.errorWrapper.appendChild(errorMsgP);
+        })
     }
 };
 
--- src/Validator.js
+++ src/Validator.js
@@ -13,13 +13,62 @@ class Validator {
             repeatPasswordError: this.repeatPasswordError
         }
     }
-    validateEmail = (email) => {}
-    emailValid = (email) => {}
-    validatePassword = (password) => {}
-    validateRepeatPassword = (password) => {}
+    validateEmail = (email) => {
+        if (this.emailValid(email)) {
+            delete this.errors.invalidEmail;
+        } else {
+            this.errors.invalidEmail = this.invalidEmail;
+        }
+    };
+    emailValid = (email) => {
+        const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/;
+        const isValid = emailRegEx.test(email);
+
+        return isValid;
+    };
+    validateUniqueEmail = (newEmail) =>{
+        const usersDB = db.getAllUsers();
+
+        let emailUnique = true;
+
+        if(usersDB.length > 0) {
+            usersDB.forEach((userObj) => {
+                if (userObj.email === newEmail) {
+                    emailUnique = false;
+                }
+            })
+            if(emailUnique) {
+                delete this.errors.emailExists;
+            } else {
+                this.errors.emailExists = this.emailExists;
+            }
+        }
+    };
+    validatePassword = (password) => {
+        if (password.length >= 6) {
+            delete this.errors.passwordError;
+        } else {
+            this.errors.passwordError = this.passwordError;
+        }
+    };
+    validateRepeatPassword = (password, passwordRepeat) => {
+    if (password === passwordRepeat) {
+        delete this.errors.repeatPasswordError;
+    } else {
+        this.errors.repeatPasswordError = this.repeatPasswordError;
+        }
+    };
 
     getErrors = () => {
         return this.errors;
+    };
+
+    resetErrors = () => {
+        this.errors = {
+            invalidEmail: this.invalidEmail,
+            passwordError: this.passwordError,
+            repeatPasswordError: this.repeatPasswordError
+        }
     }
 }
 
