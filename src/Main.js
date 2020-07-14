668ca059868a6be8ee750250cac2d4d28495e60e
Marta Jankowska <martajan218@gmail.com>
2020-07-14 23:31:18 +0200

    not working

--- src/Main.js
+++ src/Main.js
@@ -1,2 +1,138 @@
-'use strict'
+'use strict';
+
+let rockButton = document.getElementById("rock-button");
+let metalButton = document.getElementById("metal-button");
+let reggaeButton = document.getElementById("reggae-button");
+let indieButton = document.getElementById("indie-button");
+let classicalButton = document.getElementById("classical-button");
+let rapButton = document.getElementById("rap-button");
+let popButton = document.getElementById("pop-button");
+let latinButton = document.getElementById("latin-button");
+let jazzButton = document.getElementById("jazz-button");
+let electronicButton = document.getElementById("electronic-button");
+let worldButton = document.getElementById("world-button");
+let whateverButton = document.getElementById("whatever-button");
+
+const selectedTracklists = [];
+
+let rockArray = ["https://api.deezer.com/genre/133/artists", "https://api.deezer.com/genre/152/artists", "https://api.deezer.com/genre/154/artists", "https://api.deezer.com/genre/156/artists", "https://api.deezer.com/genre/160/artists"];
+let metalArray = ["https://api.deezer.com/genre/155/artists", "https://api.deezer.com/genre/464/artists"];
+let reggaeArray = ["https://api.deezer.com/genre/144/artists", "https://api.deezer.com/genre/146/artists", "https://api.deezer.com/genre/147/artists"];
+let indieArray = ["https://api.deezer.com/genre/85/artists", "https://api.deezer.com/genre/86/artists", "https://api.deezer.com/genre/87/artists", "https://api.deezer.com/genre/106/artists", "https://api.deezer.com/genre/110/artists", "https://api.deezer.com/genre/134/artists", "https://api.deezer.com/genre/154/artists", "https://api.deezer.com/genre/466/artists"];
+let classicalArray = ["https://api.deezer.com/genre/98/artists", "https://api.deezer.com/genre/99/artists", "https://api.deezer.com/genre/100/artists", "https://api.deezer.com/genre/103/artists", "https://api.deezer.com/genre/105/artists", "https://api.deezer.com/genre/173/artists", "https://api.deezer.com/genre/174/artists", "https://api.deezer.com/genre/175/artists", "https://api.deezer.com/genre/176/artists", "https://api.deezer.com/genre/179/artists"];
+let rapArray = ["https://api.deezer.com/genre/116/artists", "https://api.deezer.com/genre/166/artists", "https://api.deezer.com/genre/167/artists", "https://api.deezer.com/genre/202/artists", "https://api.deezer.com/genre/206/artists", "https://api.deezer.com/genre/208/artists"];
+let popArray = ["https://api.deezer.com/genre/95/artists", "https://api.deezer.com/genre/110/artists", "https://api.deezer.com/genre/132/artists", "https://api.deezer.com/genre/165/artists", "https://api.deezer.com/genre/168/artists"];
+let latinArray = ["https://api.deezer.com/genre/60/artists", "https://api.deezer.com/genre/67/artists", "https://api.deezer.com/genre/71/artists", "https://api.deezer.com/genre/73/artists", "https://api.deezer.com/genre/116/artists", "https://api.deezer.com/genre/122/artists", "https://api.deezer.com/genre/138/artists", "https://api.deezer.com/genre/145/artists", "https://api.deezer.com/genre/197/artists"];
+let jazzArray = ["https://api.deezer.com/genre/129/artists", "https://api.deezer.com/genre/130/artists", "https://api.deezer.com/genre/131/artists", "https://api.deezer.com/genre/153/artists", "https://api.deezer.com/genre/171/artists", "https://api.deezer.com/genre/181/artists", "https://api.deezer.com/genre/183/artists", "https://api.deezer.com/genre/184/artists", "https://api.deezer.com/genre/185/artists"];
+let electronicArray = ["https://api.deezer.com/genre/106/artists", "https://api.deezer.com/genre/108/artists", "https://api.deezer.com/genre/109/artists", "https://api.deezer.com/genre/111/artists", "https://api.deezer.com/genre/113/artists", "https://api.deezer.com/genre/115/artists"];
+let worldArray = ["https://api.deezer.com/genre/2/artists", "https://api.deezer.com/genre/16/artists", "https://api.deezer.com/genre/33/artists", "https://api.deezer.com/genre/35/artists", "https://api.deezer.com/genre/36/artists", "https://api.deezer.com/genre/37/artists", "https://api.deezer.com/genre/38/artists", "https://api.deezer.com/genre/42/artists", "https://api.deezer.com/genre/44/artists", "https://api.deezer.com/genre/48/artists", "https://api.deezer.com/genre/50/artists", "https://api.deezer.com/genre/58/artists", "https://api.deezer.com/genre/60/artists", "https://api.deezer.com/genre/67/artists", "https://api.deezer.com/genre/71/artists", "https://api.deezer.com/genre/73/artists", "https://api.deezer.com/genre/75/artists", "https://api.deezer.com/genre/81/artists", "https://api.deezer.com/genre/177/artists", "https://api.deezer.com/genre/178/artists"];
+let whateverArray = ["https://api.deezer.com/genre/0/artists"];
+
+redirectToSelection = () => {
+    setTimeout(() => location.assign('randomsongs.html'), 1000);
+  }
+
+async function fetchAll(array) {
+    const results = await Promise.all(array.map((url) => fetch(url).then((r) => r.json())
+    ));
+    return results;
+  }
+
+  let rockRandomArtist = fetchAll(rockArray)
+
+  var getMeRandomElements = function(array) {
+      var result = [];
+      for (var i = 0; i < 4; i++) {
+         result.push(array[Math.floor(Math.random()*array.length)]);
+      }
+      return result;
+  }
+
+  console.log(getMeRandomElements(rockRandomArtist));
+
+/*async function fetchAll(array) {
+    const results = await Promise.all(array.map((url) => fetch(url)
+    .then((r) => r.json())
+    .then(data => {
+        getArtists = [];
+        let allArtists = data.data[Math.floor(Math.random() * data.data.length)];
+        getArtists.push(allArtists);
+        getArtists.forEach((item) => getTracks(item))
+        })
+    ));
+  };
+
+function getTracks(item) {
+    fetch(array + item.tracklist)
+    .then(r => r.json())
+    .then(data => {
+        const tracks = data.data;
+        tracks.length = 4;
+        tracks.forEach(myTrack => {
+            selectedTracklists.push(myTrack.link.slice(29));
+            songsList.innerHTML = "";
+        })
+    })
+    .then (() => {
+        let finalList = document.querySelector(".songs-list");
+        finalList.innerHTML = "";
+        selectedTracklists.forEach(link => {
+            let songsList = document.createElement('li');
+            songsList.innerHTML = `<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=007FEB&layout=dark&size=medium&type=tracks&id=${link}&app_id=1" width="300" height="300"></iframe>`;
+            selectedTracklists.pop();
+            finalList.appendChild(songsList);
+        })
+        redirectToSelection();
+    })
+    .catch(error => console.error(error));
+  };*/
+
+
+/*rockButton.addEventListener("click", function(){
+    fetchAll(rockArray[Math.floor(Math.random()*rockArray.length)]);
+});
+metalButton.addEventListener("click", function(){
+    fetchAll(metalArray[Math.floor(Math.random()*metalArray.length)])
+});
+reggaeButton.addEventListener("click", function(){
+    fetchAll(reggaeArray[Math.floor(Math.random()*reggaeArray.length)])
+});
+indieButton.addEventListener("click", function(){
+    fetchAll(indieArray[Math.floor(Math.random()*indieArray.length)])
+});
+classicalButton.addEventListener("click", function(){
+    fetchAll(classicalArray[Math.floor(Math.random()*classicalArray.length)])
+});
+rapButton.addEventListener("click", function(){
+    fetchAll(rapArray[Math.floor(Math.random()*rapArray.length)])
+});
+popButton.addEventListener("click", function(){
+    fetchAll(popArray[Math.floor(Math.random()*popArray.length)])
+});
+latinButton.addEventListener("click", function(){
+    fetchAll(latinArray[Math.floor(Math.random()*latinArray.length)])
+});
+jazzButton.addEventListener("click", function(){
+    fetchAll(jazzArray[Math.floor(Math.random()*jazzArray.length)])
+});
+electronicButton.addEventListener("click", function(){
+    fetchAll(electronicArray[Math.floor(Math.random()*electronicArray.length)])
+});
+worldButton.addEventListener("click", function(){
+    fetchAll(worldArray[Math.floor(Math.random()*worldArray.length)])
+});
+whateverButton.addEventListener("click", function(){
+    fetchAll(whateverArray[Math.floor(Math.random()*whateverArray.length)])
+});*/
+
+rockButton.addEventListener("click", function(){
+    redirectToSelection();
+    fetchAll(rockArray);
+});
+
+
+
+
+
+
 
